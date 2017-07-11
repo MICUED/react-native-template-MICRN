c675ac8884d22342f96d2845a70e0e2159b1a371
necfol <necfoll@gmail.com>
2017-07-11 10:33:21 +0800

    add navigation transitionConfig

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { StackNavigator } from 'react-navigation';
-import HomeScreen from './container/Home.js';
-
+import React, { Component } from 'react'
+import { StackNavigator } from 'react-navigation'
+import HomeScreen from './container/Home.js'
+import QRScanScreen from './container/QRScan.js'
 const paramsToProps = (SomeComponent) => { 
     return class extends Component {
-        static navigationOptions = SomeComponent.navigationOptions;
+        static navigationOptions = SomeComponent.navigationOptions
         render() {
             const {navigation, ...otherProps} = this.props
             const {state: {params}} = navigation
@@ -14,9 +14,27 @@ const paramsToProps = (SomeComponent) => {
 }
 
 export const AppNavigator = StackNavigator({
-  Home: { screen: paramsToProps(HomeScreen) }
-},
-  {
+    Home: { screen: paramsToProps(HomeScreen) },
+    QRScan: {screen: paramsToProps(QRScanScreen)},
+  },{
+    transitionConfig: () => ({
+        screenInterpolator: sceneProps => {
+            const { layout, position, scene } = sceneProps
+            const { index } = scene
+
+            const translateX = position.interpolate({
+                inputRange: [index - 1, index, index + 1],
+                outputRange: [layout.initWidth, 0, 0]
+            })
+
+            const opacity = position.interpolate({
+                inputRange: [index - 1, index - 0.99, index, index + 0.99, index + 1],
+                outputRange: [0, 1, 1, 0.3, 0]
+            })
+
+            return { opacity, transform: [{ translateX }] }
+        }
+    }),
     mode: 'card',
     headerMode: 'none'
-  });
+  })
