ed1ca8f63b60fa05afe1ad1376b21c854fef663a
necfol <necfoll@gmail.com>
2017-07-11 10:01:05 +0800

    change androidback && add react-native-camera dependencies

--- src/root.js
+++ src/root.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { BackHandler } from 'react-native'
 import { addNavigationHelpers } from 'react-navigation'
 import { AppNavigator } from './routerConfig.js'
 @connect(state => ({
   nav: state.nav
 }), dispatch => ({dispatch}))
 export default class AppWithNavigationState extends Component {
+  handleBackPress = () => {
+    const { dispatch, nav } = this.props;
+    const navigation = addNavigationHelpers({
+      dispatch,
+      state: nav,
+    });
+    navigation.goBack();
+    return true;
+  }  
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+  }
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
+  }  
   render() {
     var { dispatch, nav } = this.props
     return (
